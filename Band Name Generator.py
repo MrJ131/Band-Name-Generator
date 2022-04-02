5fe34b29c55638541cc6bc938266883b692f3a21
MrJ131 <78934933+MrJ131@users.noreply.github.com>
2022-04-02 23:30:19 +0100

    Add files via upload

---
+++ Band Name Generator.py
@@ -0,0 +1,9 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print ("Welcome to the Band Name Generator")
+
+city = input ("What city did you grow up in?\n")
+
+pet = input ("What was the name of your first pet?\n")
+
+print ("Your band name could be " + city + " " + pet)
